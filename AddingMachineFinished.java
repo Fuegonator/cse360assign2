4ab7cd01aa17fd7b155b6526a77548c405f0c8cd
Fuegonator <54912434+Fuegonator@users.noreply.github.com>
2019-10-08 15:03:37 -0700

    Add files via upload

--- AddingMachineFinished.java
+++ AddingMachineFinished.java
@@ -9,37 +9,37 @@
 
 package cse360assign2;
 
-public class AddingMachineFinished {
+public class AddingMachineFinished {//This class contains methods to run various 
+									//mathematical operations.
 	
-	private int total;
-	
-	private String line = "0";
+	private int total;//Global variable that holds the total number
+	private String line = "0";//String line that hold the history of operations
 	
 	public AddingMachineFinished () {
 		total = 0;  // not needed - included for clarity
 	}
 	
-	public int getTotal () {
+	public int getTotal () {//This method return the current total
 		return total;
 	}
 	
-	public void add (int value) {
+	public void add (int value) {//This method adds the passed integer to the total amount
 		String str = Integer.toString(value);
-		line += "+" + str;
+		line += " + " + str;
 		total += value;
 	}
 	
-	public void subtract (int value) {
+	public void subtract (int value) {//This method subtracts the passed integer to the total amount
 		String str = Integer.toString(value);
-		line += "-" + str;
+		line += " - " + str;
 		total -= value;
 	}
 		
-	public String toString () {
+	public String toString () {//This method returns the operation history
 		return line;
 	}
 
-	public void clear() {
+	public void clear() {//This clears the history and the total.
 	line = "0";
 	total = 0;
 	}
